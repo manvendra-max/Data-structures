73222d350de7b9d653bbc754a677cdc2dbe313c0
Manvendra <singhchouhanmanvendra44@gmail.com>
2022-04-06 10:16:15 +0530

    commit 2

--- printNIntegers.py
+++ printNIntegers.py
@@ -1,14 +1,5 @@
 n = int(input())
 
-# def rec(n):
-#     if n==0:
-#         print(0)
-#     else:
-#         rec(n-1)
-#         print(n)
-
-# rec(n)
-
 def rec(n):
     if n ==0 :
         print(0)
