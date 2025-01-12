1335755b2f8d3bca975ccb97163cd96d2aedcd24
manvendra-max <singhchouhanmanvendra44@gmail.com>
2025-01-13 00:53:34 +0530

    add logic for best case scenario in bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,8 +1,12 @@
 def bubble_sort(inp):
     for i in range(len(inp)-1):
+        is_swapped = 0 #for best case scenario
         for j in range(len(inp)-i-1):
             if inp[j] > inp[j+1]:
                 inp[j], inp[j+1] = inp[j+1], inp[j]
+                is_swapped = 1
+        if is_swapped == 0:
+            break
     return inp
 
 input_list = [1, 0, -98, 34,678,876876, 0,0,5,1,999999, 2]
