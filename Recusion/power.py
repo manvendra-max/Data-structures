3e74be1b9d5db558620e77e35e2e5fd276d02f3b
manvendra-max <singhchouhanmanvendra44@gmail.com>
2025-05-19 14:25:18 +0530

    optimised the power method

--- Recusion/power.py
+++ Recusion/power.py
@@ -8,3 +8,21 @@ def power(n,x):
     s = n * power(n,x-1)
     return s
 print(power(num, pow))
+
+# optimised version
+# exponentiation by squaring
+def pow(x, n):
+    ans = 1
+    nn = n
+    while nn > 0:
+        if nn%2 == 0:
+            # If nn is even, the algorithm can simplify the computation by 
+            # squaring the base and halving the exponent.
+            x = x * x
+            nn = nn//2
+        else:
+            ans = ans * x
+            nn -= 1
+    return ans
+
+print(pow(2,3))
